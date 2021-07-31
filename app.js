a27efa978fd143fb604be2b822d1c0f1a2dd8962
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2021-07-31 15:08:22 -0400

    made changes for heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const cors = require("cors");
+const path = require("path");
 
 // Connect to MongoDB
 const db = process.env.mongoURI || require("./config/keys").mongoURI;
@@ -46,6 +47,15 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/tasks", require("./routes/tasks"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 // Run Server
 const port = process.env.PORT || 5000;
