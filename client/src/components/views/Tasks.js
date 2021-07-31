9228914b0fd0a3620604e2f70267293b9374fa81
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2021-07-31 15:24:47 -0400

    fixed title and tasks view

--- client/src/components/views/Tasks.js
+++ client/src/components/views/Tasks.js
@@ -25,18 +25,12 @@ const Tasks = () => {
 	return (
 		<div>
 			<br />
-			{cachedTasks &&
-				cachedTasks.length &&
-				cachedTasks.map((task) => <Task key={task._id} task={task} />)}
+			{cachedTasks && cachedTasks.length
+				? cachedTasks.map((task) => <Task key={task._id} task={task} />)
+				: ""}
 			<br />
 		</div>
 	);
 };
-
-// const classes = {
-// 	generalBox:
-// 		"w-min mx-auto rounded-2xl border-4 border-purple-200 border-opacity-90 bg-white text-gray-700 filter drop-shadow-md",
-// 	loadingBox: "py-4 pl-3 pr-4",
-// };
 
 export default Tasks;
